ce70b818fd24cf8a819c6454863b0520a289b04b
Thomas Martinez <tmartinez114@gmail.com>
2020-05-03 01:25:48 -0400

    renamed the class to EntryPoint to resolve subclass issue name

---
+++ cSharp/SetUpEnvironment/SetUpEnvironment/SetUpEnvironment/EntryPoint.cs
@@ -0,0 +1,18 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl("http://testing.todorvachev.com");
+
+        Thread.Sleep(3000);
+
+        driver.Quit();
+    }
+}
