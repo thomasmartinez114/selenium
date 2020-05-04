5e66a1a8fcd9bf414fa0583eb1a649158a8abd63
Thomas Martinez <tmartinez114@gmail.com>
2020-05-04 00:30:45 -0400

    push - moving on to java udemy course

--- cSharp/SetUpEnvironment/SetUpEnvironment/SetUpEnvironment/EntryPoint.cs
+++ cSharp/SetUpEnvironment/SetUpEnvironment/SetUpEnvironment/EntryPoint.cs
@@ -6,10 +6,8 @@ class EntryPoint
 {
     static void Main()
     {
-        IWebDriver driver = new InternetExplorerDriver();
-
-        // driver.Navigate().GoToUrl("http://testing.todorvachev.com");
-        driver.Navigate().GoToUrl("http://www.google.com");
-        driver.FindElement(By.Name("q")).SendKeys("Learn Automation");
+        var driver = new InternetExplorerDriver();
+        driver.Navigate().GoToUrl("http://bing.com");
+        Console.Read();
     }
 }
