773255565fbcf951d97e398b08da3ac97c61f43d
Thomas Martinez <tmartinez114@gmail.com>
2020-05-04 00:05:41 -0400

    changed Crhome driver to use IE Driver

--- cSharp/SetUpEnvironment/SetUpEnvironment/SetUpEnvironment/EntryPoint.cs
+++ cSharp/SetUpEnvironment/SetUpEnvironment/SetUpEnvironment/EntryPoint.cs
@@ -1,42 +1,15 @@
-﻿/*
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using System.Threading;
+﻿using OpenQA.Selenium;
+//using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
 
 class EntryPoint
 {
     static void Main()
     {
-        IWebDriver driver = new ChromeDriver();
+        IWebDriver driver = new InternetExplorerDriver();
 
-        driver.Navigate().GoToUrl("http://testing.todorvachev.com");
-
-        IWebElement element = driver.FindElement(By.Name("entry-header"));
-
-        Thread.Sleep(3000);
-
-        driver.Quit();
-    }
-}
-*/
-
-using System;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Support.UI;
-using SeleniumExtras.WaitHelpers;
-
-class HelloSelenium
-{
-    static void Main()
-    {
-        using (IWebDriver driver = new FirefoxDriver())
-        {
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-            driver.Navigate().GoToUrl("https://www.google.com/ncr");
-            driver.FindElement(By.Name("q")).SendKeys("cheese" + Keys.Enter);
-            IWebElement firstResult = wait.Until(ExpectedConditions.ElementExists(By.CssSelector("h3>div")));
-            Console.WriteLine(firstResult.GetAttribute("textContent"));
-        }
+        // driver.Navigate().GoToUrl("http://testing.todorvachev.com");
+        driver.Navigate().GoToUrl("http://www.google.com");
+        driver.FindElement(By.Name("q")).SendKeys("Learn Automation");
     }
 }
