5884ca9b31c44c3a73ec21f6bc044c4ff4f65208
Thomas Martinez <tmartinez114@gmail.com>
2020-05-06 01:19:59 -0400

    Creating first test class - comments for what the method will execute

--- java/selenium-for-beginners/src/test/java/com/herokuapp/internet/PositiveTests.java
+++ java/selenium-for-beginners/src/test/java/com/herokuapp/internet/PositiveTests.java
@@ -1,5 +1,18 @@
 package com.herokuapp.internet;
 
 public class PositiveTests {
-
+	
+	public void loginTest() {
+//		   Create driver
+//		   open test page
+//		   enter username
+//		   enter password
+//		   click login button
+//		   
+//		   verifications:
+//			   new url
+//			   logout button is visible
+//			   successful login message
+	}
+	
 }
