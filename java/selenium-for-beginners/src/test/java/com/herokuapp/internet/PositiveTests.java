cd272c3f6536ef47287b570fc12cd1a4b7c69c26
Thomas Martinez <tmartinez114@gmail.com>
2020-05-06 01:24:31 -0400

    Set the chrome driver and init the WebDriver obj

--- java/selenium-for-beginners/src/test/java/com/herokuapp/internet/PositiveTests.java
+++ java/selenium-for-beginners/src/test/java/com/herokuapp/internet/PositiveTests.java
@@ -4,8 +4,16 @@ public class PositiveTests {
 	
 	public void loginTest() {
 //		   Create driver
+		   System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+		   WebDriver driver = new ChromeDriver();
+		
 //		   open test page
+		
+		
 //		   enter username
+		
+		
+		
 //		   enter password
 //		   click login button
 //		   
