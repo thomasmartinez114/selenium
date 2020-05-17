37a34e0fb925cdbcf7a64b2f97fce66635aa635b
Thomas Martinez <tmartinez114@gmail.com>
2020-05-17 01:19:43 -0400

    sleep() private method added to code

--- java/selenium-for-beginners/src/test/java/com/herokuapp/internet/NegativeTests.java
+++ java/selenium-for-beginners/src/test/java/com/herokuapp/internet/NegativeTests.java
@@ -7,38 +7,47 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class NegativeTests {
-	
+
 	@Test
 	public void negativeUsernameTest() {
 		System.out.println("Starting negativeUsernameTest");
-		
+
 		// Create driver
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		
+
 		// sleep for 3 seconds
 		sleep(3000);
-		
+
 		// maximize browser window
 		driver.manage().window().maximize();
-		
+
 		// open page
 		String url = "http://the-internet.herokuapp.com/login";
 		driver.get(url);
 		System.out.println("Page is open");
-		
+
 		// enter incorrect username
 		WebElement username = driver.findElement(By.id("username"));
 		username.sendKeys("tmartinez");
-		
+
 		// enter password
 		WebElement password = driver.findElement(By.className("password"));
 		password.sendKeys("SuperSecretPassword!");
-		
+
 		// click login button
 		WebElement loginButton = driver.findElement(By.tagName("button"));
 		loginButton.click();
-		
+
+	}
+
+	private void sleep(long m) {
+		try {
+			Thread.sleep(m);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
