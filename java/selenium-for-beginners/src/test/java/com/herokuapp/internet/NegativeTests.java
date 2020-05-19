b471366097ec26048bfba21ba15b41fa45ea1ad7
Thomas Martinez <tmartinez114@gmail.com>
2020-05-19 00:27:25 -0400

    removed the import of chrome dirver since not used

--- java/selenium-for-beginners/src/test/java/com/herokuapp/internet/NegativeTests.java
+++ java/selenium-for-beginners/src/test/java/com/herokuapp/internet/NegativeTests.java
@@ -3,7 +3,6 @@ package com.herokuapp.internet;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
