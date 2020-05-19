4b73bc62c00d5950df3f1e18b2287ff9543394b4
Thomas Martinez <tmartinez114@gmail.com>
2020-05-18 22:50:17 -0400

    added method test for incorrectPasswordTest()

--- java/selenium-for-beginners/src/test/java/com/herokuapp/internet/NegativeTests.java
+++ java/selenium-for-beginners/src/test/java/com/herokuapp/internet/NegativeTests.java
@@ -55,6 +55,53 @@ public class NegativeTests {
 		driver.quit();
 
 	}
+	
+		@Test
+		public void incorrectPasswordTest() {
+			System.out.println("Starting negativeUsernameTest");
+
+			// Create driver
+			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+			WebDriver driver = new ChromeDriver();
+
+			// sleep for 3 seconds
+			sleep(3000);
+
+			// maximize browser window
+			driver.manage().window().maximize();
+
+			// open page
+			String url = "http://the-internet.herokuapp.com/login";
+			driver.get(url);
+			System.out.println("Page is open");
+
+			// enter incorrect username
+			WebElement username = driver.findElement(By.id("username"));
+			username.sendKeys("tomsmith");
+
+			// enter password
+			WebElement password = driver.findElement(By.name("password"));
+			password.sendKeys("incorrectpassword");
+
+			// click login button
+			WebElement loginButton = driver.findElement(By.tagName("button"));
+			loginButton.click();
+			
+			sleep(3000);
+			
+			// invalid username error
+			WebElement invalidMessage = driver.findElement(By.xpath("//div[@id='flash']"));
+			String expectedMessage = "Your password is invalid!";
+			String actualMessage = invalidMessage.getText();
+			// Assertion to compare
+			Assert.assertTrue(actualMessage.contains(expectedMessage),
+					"Actual message does not contain the expected message.\nActual Message: " + actualMessage
+					+ "\nExpected Message: " + expectedMessage);
+			
+			// Close browser
+			driver.quit();
+
+		}
 
 	private void sleep(long m) {
 		try {
