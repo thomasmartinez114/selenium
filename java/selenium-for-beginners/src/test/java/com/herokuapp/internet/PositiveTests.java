7570f30a5718894383230472ddb54c0bf0bcded2
Thomas Martinez <tmartinez114@gmail.com>
2020-05-19 23:27:33 -0400

    moved negativeLoginTest() from NegativeTests class to PositiveTests class

--- java/selenium-for-beginners/src/test/java/com/herokuapp/internet/PositiveTests.java
+++ java/selenium-for-beginners/src/test/java/com/herokuapp/internet/PositiveTests.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class PositiveTests {
@@ -66,6 +68,58 @@ public class PositiveTests {
 						+ "\nExpected Message: " + expectedMessage);
 
 //		   Close browser
+		driver.quit();
+
+	}
+	
+	@Parameters({ "username", "password", "expectedMessage" })
+	@Test(priority = 1)
+	public void negativeLoginTest(String username, String password, String expectedErrorMessage) {
+		System.out.println("Starting negativeLoginTest with " + username + " and " + password);
+
+		// Create driver
+		// System.setProperty("webdriver.chrome.driver",
+		// "src/main/resources/chromedriver.exe");
+		// WebDriver driver = new ChromeDriver();
+
+		// Create driver for Firefox
+		System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+
+		// sleep for 3 seconds
+		sleep(3000);
+
+		// maximize browser window
+		driver.manage().window().maximize();
+
+		// open page
+		String url = "http://the-internet.herokuapp.com/login";
+		driver.get(url);
+		System.out.println("Page is open");
+
+		// enter incorrect username
+		WebElement usernameElement = driver.findElement(By.id("username"));
+		usernameElement.sendKeys(username);
+
+		// enter password
+		WebElement passwordElement = driver.findElement(By.name("password"));
+		passwordElement.sendKeys(password);
+
+		// click login button
+		WebElement loginButton = driver.findElement(By.tagName("button"));
+		loginButton.click();
+
+//		sleep(3000);
+
+		// invalid username error
+		WebElement invalidMessage = driver.findElement(By.xpath("//div[@id='flash']"));
+		String actualMessage = invalidMessage.getText();
+		// Assertion to compare
+		Assert.assertTrue(actualMessage.contains(expectedErrorMessage),
+				"Actual message does not contain the expected message.\nActual Message: " + actualMessage
+						+ "\nExpected Message: " + expectedErrorMessage);
+
+		// Close browser
 		driver.quit();
 
 	}
