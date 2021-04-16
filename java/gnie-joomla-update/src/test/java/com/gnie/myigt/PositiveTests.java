6a9163a6e6ff0e0e4b56a85beabadf9baf06df7d
Thomas Martinez <tmartinez114@gmail.com>
2021-04-15 23:08:23 -0400

    set up Test

---
+++ java/gnie-joomla-update/src/test/java/com/gnie/myigt/PositiveTests.java
@@ -0,0 +1,22 @@
+package com.gnie.myigt;
+
+public class PositiveTests {
+
+	public void loginTest() {
+		
+		// Create Edge Driver
+		
+		// Open test page
+		
+		// Enter username
+		
+		// Enter password
+		
+		// Click login button
+		
+		// Verification:
+			// new url
+		
+	}
+	
+}
