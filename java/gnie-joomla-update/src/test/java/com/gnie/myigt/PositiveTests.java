20a5946c9b6ace9b9a32c9ee7be0a7e43855422d
Thomas Martinez <tmartinez114@gmail.com>
2021-04-16 23:07:14 -0400

    added edge web driver and imported

--- java/gnie-joomla-update/src/test/java/com/gnie/myigt/PositiveTests.java
+++ java/gnie-joomla-update/src/test/java/com/gnie/myigt/PositiveTests.java
@@ -1,10 +1,16 @@
 package com.gnie.myigt;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 public class PositiveTests {
 
+	
 	public void loginTest() {
 		
 		// Create Edge Driver
+		System.setProperty("webdriver.edge.driver", "src/main/resources/msedgedriver.exe");
+		WebDriver driver = new EdgeDriver();
 		
 		// Open test page
 		
