039b276ab8debf6114c368fe0930d5e1fb1e74ef
Thomas Martinez <tmartinez114@gmail.com>
2021-04-17 08:33:35 -0400

    joomlaUpdate variable

---
+++ java/gnie-joomla-update/src/test/java/com/gnie/myigt/Looping.java
@@ -0,0 +1,29 @@
+package com.gnie.myigt;
+
+public class Looping {
+
+	public static void main(String[] args) {
+
+	// TODO Auto-generated method stub
+
+		String[] sites = { "https://gnieqa.myigt.com/sites/us/cc/administrator/index.php",
+				"https://gnieqa.myigt.com/sites/us/dca/administrator/index.php",
+				"https://gnieqa.myigt.com/sites/us/fst/administrator/index.php",
+				"https://gnieqa.myigt.com/sites/us/rrf/administrator/index.php",
+				"https://gnieqa.myigt.com/sites/us/sd/administrator/index.php",
+				"https://gnieqa.myigt.com/sites/us/to/administrator/index.php",
+				"https://sch.gnieqa.myigt.com/schedule/administrator/index.php"
+		};
+
+		for (int instance = 0; instance < sites.length; instance++) {
+			
+			String gnieSite = sites[instance];
+
+			// Set Joomla Update Button
+			String joomlaUpdate = "document.location=" + gnieSite + "?option=com_joomlaupdate";
+
+			System.out.println(joomlaUpdate);
+
+		}
+}
+}
--- java/gnie-joomla-update/src/test/java/com/gnie/myigt/PositiveTests.java
+++ java/gnie-joomla-update/src/test/java/com/gnie/myigt/PositiveTests.java
@@ -10,63 +10,70 @@ public class PositiveTests {
 
 	@Test
 	public void loginTest() {
-		
-		String[] sites = {
-				"https://gnieqa.myigt.com/sites/us/cc/administrator/index.php", 
-				"https://gnieqa.myigt.com/sites/us/dca/administrator/index.php", 
-				"https://gnieqa.myigt.com/sites/us/fst/administrator/index.php", 
-				"https://gnieqa.myigt.com/sites/us/rrf/administrator/index.php", 
-				"https://gnieqa.myigt.com/sites/us/sd/administrator/index.php", 
-				"https://gnieqa.myigt.com/sites/us/to/administrator/index.php", 
-				"https://sch.gnieqa.myigt.com/schedule/administrator/index.php"};
-		
+
+		String[] sites = { "https://gnieqa.myigt.com/sites/us/cc/administrator/index.php",
+				"https://gnieqa.myigt.com/sites/us/dca/administrator/index.php",
+				"https://gnieqa.myigt.com/sites/us/fst/administrator/index.php",
+				"https://gnieqa.myigt.com/sites/us/rrf/administrator/index.php",
+				"https://gnieqa.myigt.com/sites/us/sd/administrator/index.php",
+				"https://gnieqa.myigt.com/sites/us/to/administrator/index.php",
+				"https://sch.gnieqa.myigt.com/schedule/administrator/index.php" };
+
 		for (int instance = 0; instance < sites.length; instance++) {
-		
-		System.out.println("Starting login");
 
-		// Create Chrome Driver
-		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+			// setting String variable for gnieSite
+			String gnieSite = sites[instance];
+			System.out.println(gnieSite);
 
-		// Maximize browser window
-		driver.manage().window().maximize();
+			System.out.println("Starting login");
 
-		// Open test page
-		String url = sites[instance];
-		driver.get(url);
-		System.out.println("Browser as been open.");
-		
-		// Log into Joomla Admin
+			// Create Chrome Driver
+			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+			WebDriver driver = new ChromeDriver();
 
-		// Enter username
-		WebElement username = driver.findElement(By.id("mod-login-username"));
-		username.sendKeys("");
+			// Maximize browser window
+			driver.manage().window().maximize();
 
-		// Enter password
-		WebElement password = driver.findElement(By.id("mod-login-password"));
-		password.sendKeys("");
+			// Open test page
+			String url = sites[instance];
+			driver.get(url);
+			System.out.println("Browser as been open.");
 
-		// Click login button
-		WebElement loginButton = driver.findElement(By.tagName("button"));
-		loginButton.click();
-		
-		// sleep
-		sleep(8000);
-		
-		// Click Joomla Update Button
+			// Log into Joomla Admin
+
+			// Enter username
+			WebElement username = driver.findElement(By.id("mod-login-username"));
+			username.sendKeys("");
+
+			// Enter password
+			WebElement password = driver.findElement(By.id("mod-login-password"));
+			password.sendKeys("");
+
+			// Click login button
+			WebElement loginButton = driver.findElement(By.tagName("button"));
+			loginButton.click();
+
+			// sleep
+			sleep(8000);
+
+			// Set Joomla Update Button
+			String joomlaUpdate = "document.location=" + gnieSite + "?option=com_joomlaupdate";
+
+			// Click Joomla Update Button
 //		WebElement updateButton = driver.findElement(By.xpath("//div[@id='system-message-container']/div[1]/button[@class='btn btn-primary']"));
-		WebElement updateButton = driver.findElement(By.xpath('document.location='+site[instance]+'?option=com_joomlaupdate'))
-		updateButton.click();
-		
-		// sleep
-		sleep(3000);
-		
-		// Click to install update
-		WebElement installButton = driver.findElement(By.xpath("/html//form[@id='adminForm']//table[@class='table table-striped']//button[@class='btn btn-primary']"));
-		installButton.click();
-		
+			WebElement updateButton = driver.findElement(By.xpath(joomlaUpdate));
+			updateButton.click();
+
+			// sleep
+			sleep(3000);
+
+			// Click to install update
+			WebElement installButton = driver.findElement(By.xpath(
+					"/html//form[@id='adminForm']//table[@class='table table-striped']//button[@class='btn btn-primary']"));
+			installButton.click();
+
 		}
-		
+
 //		// sleep for install
 //		sleep(600000);
 //		
@@ -75,7 +82,6 @@ public class PositiveTests {
 //		
 //		// Close Browser
 //		driver.quit();
-		
 
 	}
 
