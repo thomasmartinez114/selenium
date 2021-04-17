99fb1712ac940fa2b3ff3bc97600e52dc166a94c
Thomas Martinez <tmartinez114@gmail.com>
2021-04-16 23:51:55 -0400

    added username/password/login elements to search for

--- java/gnie-joomla-update/src/test/java/com/gnie/myigt/PositiveTests.java
+++ java/gnie-joomla-update/src/test/java/com/gnie/myigt/PositiveTests.java
@@ -1,6 +1,8 @@
 package com.gnie.myigt;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.Test;
 
@@ -25,13 +27,18 @@ public class PositiveTests {
 		
 		// sleep for 3 seconds to view action
 		sleep(3000);
+		
+		// Log into Joomla Admin
 
 		// Enter username
+		WebElement username = driver.findElement(By.id("mod-login-username"));
 
 		// Enter password
+		WebElement password = driver.findElement(By.id("mod-login-password"));
 
 		// Click login button
-
+		WebElement loginButton = driver.findElement(By.tagName("button"));
+				
 		// Verification:
 		// new url
 		
